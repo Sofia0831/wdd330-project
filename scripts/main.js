73485587d637630dd394b7b4520fb754600cab2a
sofiapantas <sofiasarabia0831@gmail.com>
2025-04-04 17:42:39 +0800

    html and css

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { updateFooter } from "./utils.js";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const footerElement = document.querySelector("footer");
+    updateFooter(footerElement);
+
+});
---
+++ scripts/utils.js
@@ -0,0 +1,9 @@
+export function updateFooter(footerElement) {
+    const currentYear = new Date().getFullYear();
+    const lastModifiedDate = document.lastModified;
+  
+    const footerParagraphs = footerElement.querySelectorAll("p");
+      footerParagraphs[0].textContent = `Copyright © ${currentYear} SOFIA PANTAS | WDD330 PROJECT`;
+      footerParagraphs[1].textContent = `Last modified: ${lastModifiedDate}`;
+  
+  }
